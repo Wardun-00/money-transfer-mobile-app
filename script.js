ca74f26b4b9c1dc96971bc2e889657525fa03263
Wardun-00 <wardun.bd@gmial.com>
2025-08-23 06:08:37 +0600

    added home page using button click

---
+++ script.js
@@ -0,0 +1,33 @@
+// Login button Functionality
+
+document.getElementById('loginButton').addEventListener('click', function(e){
+    e.preventDefault()
+   
+    const mobileNumber = 1318019082;
+    const pinNumber = 8687;
+// mobile number
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+    // console.log(mobileNumberValueConvert)
+
+    // pin number
+
+    const pinNumberValue = document.getElementById('pin-number').value;
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    // console.log(mobileNumberValueConverted, pinNumberValueConverted)
+
+    // number and pin compare (number and pin match korle kaz hobe)
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        //next home page a jaowar jonno 
+        window.location.href='./home.html';
+    }
+    else{
+        //alert dibe vul hole
+        alert('Invalid Credentials');
+    }
+
+
+})
